796890ff63dce658bd15530938edd481ac32580e
Chandranath Mondal <chandranath222@gmail.com>
2023-02-01 22:24:12 +0530

    Added params route for reading cft template for s3

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 # Entry point to the api
 from flask import Flask
+import json
+from controller.get_parameters import read_yaml_file
 
 app = Flask(__name__)
 
 # Define routes
 
 # Test route
+
+
 @app.route('/')
 def home():
     return ('Working container')
+
+# Get params for S3 cloudformation template
+
+
+@app.route('/params')
+def params():
+    res = {}
+    res['params'] = read_yaml_file()
+    print(res)
+    return (json.dumps(res))
+
 
 if __name__ == '__main__':
     app.run()
---
+++ controller/get_parameters.py
@@ -0,0 +1,23 @@
+import yaml
+
+# Constructor to add support for tags to safe_load constructor
+
+
+def any_constructor(loader, tag_suffix, node):
+    if isinstance(node, yaml.MappingNode):
+        return loader.construct_mapping(node)
+    if isinstance(node, yaml.SequenceNode):
+        return loader.construct_sequence(node)
+    return loader.construct_scalar(node)
+
+
+yaml.add_multi_constructor('', any_constructor, Loader=yaml.SafeLoader)
+
+
+def read_yaml_file():
+    with open('C:/Users/chand/Documents/Projects/cft-genrator/cft-generator/controller/s3_bucket.yaml', 'r') as file:
+        parameters = yaml.safe_load(file)
+        return parameters['Parameters']
+
+
+# print(read_yaml_file())
